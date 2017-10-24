8609dcf76d93c3e8244a4a138ec77f20ec90b18a
Athiyaman Muthukumarasamy <Athi.Muthukumarasamy@medpro.com>
2017-10-24 15:50:06 -0400

    Solutions for Chase Digital

--- javajdk18/src/org/training/chase/ChaseDigital.java
+++ javajdk18/src/org/training/chase/ChaseDigital.java
@@ -7,7 +7,14 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -15,26 +22,102 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+class StringTask implements Callable<String>{
+	public StringTask() {
+	}
+
+	public String call(){
+			Thread.currentThread().setName("Thread " + Thread.currentThread().getId());
+			return Thread.currentThread().getName() + " : Login complete at "+LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)+".";
+		};
+}
+
+
 public class ChaseDigital {
 	public static void main(String[] args) {
-		//LocalDateTime lt = LocalDateTime.now();
-		//String asIsoDateTime = lt.format(DateTimeFormatter.ISO_DATE_TIME);
-		System.out.println("Login complete at "+LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)+".");
+		// ############TEST 1 ###################
+		//System.out.println("Login complete at "+LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)+".");
 		
-		//String titles = "";
+		createAuditLog();
 		
-		String[] titleArr = getMovieTitles("of");
-		System.out.println(Arrays.asList(titleArr));
-       	
+		/*try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+		// ############TEST 2 ###################
+		/*String[] titleArr = getMovieTitles("of");
+		System.out.println(Arrays.asList(titleArr));*/
+		
+		String jsonResp = "{\"page\":1,\"per_page\":10,\"total\":375,\"total_pages\":38,\"data\":[{\"Poster\":\"N/A\",\"Title\":\"The Making of 'Waterworld'\",\"Type\":\"movie\",\"Year\":1995,\"imdbID\":\"tt2670548\"},{\"Poster\":\"N/A\",\"Title\":\"Waterworld 4: History of the Islands\",\"Type\":\"movie\",\"Year\":1997,\"imdbID\":\"tt0161077\"},{\"Poster\":\"N/A\",\"Title\":\"Behind the Scenes of the Most Fascinating Waterworld on Earth: The Great Backwaters, Kerala.\",\"Type\":\"movie\",\"Year\":2014,\"imdbID\":\"tt5847056\"},{\"Poster\":\"N/A\",\"Title\":\"Fighting, Flying and Driving: The Stunts of Spiderman 3\",\"Type\":\"movie\",\"Year\":2007,\"imdbID\":\"tt1132238\"},{\"Poster\":\"N/A\",\"Title\":\"The Death of Spiderman\",\"Type\":\"movie\",\"Year\":2015,\"imdbID\":\"tt5921428\"},{\"Poster\":\"https://images-na.ssl-images-amazon.com/images/M/MV5BN2Q0NjRiYTAtNDJiZC00ODgyLWI4NDMtYzRmYjY3MjZjMmQ0XkEyXkFqcGdeQXVyNTQzMjAxMzA@._V1_SX300.jpg\",\"Title\":\"The Superman/Aquaman Hour of Adventure\",\"Type\":\"series\",\"Year\":1967,\"imdbID\":\"tt0231046\"},{\"Poster\":\"N/A\",\"Title\":\"Aquaman: War of the Water Worlds\",\"Type\":\"game\",\"Year\":1996,\"imdbID\":\"tt1064720\"},{\"Poster\":\"N/A\",\"Title\":\"Aquaman: Sovereign of the Seas\",\"Type\":\"movie\",\"Year\":2007,\"imdbID\":\"tt1186634\"},{\"Poster\":\"N/A\",\"Title\":\"Judge Rummy's Off Day\",\"Type\":\"movie\",\"Year\":1918,\"imdbID\":\"tt0370820\"},{\"Poster\":\"N/A\",\"Title\":\"The Rummy Act of Omar K.M.\",\"Type\":\"movie\",\"Year\":1916,\"imdbID\":\"tt0496146\"}]}";       	
+		String[] titles = returnMovieTitles(jsonResp);		
+		System.out.println ("Sorted Movie Array : " + Arrays.toString(titles));
+		System.out.println ("Array size = " + titles.length);
+		
+		// ############TEST 3 ###################
 		String str ="aeiaaioooaauuaeiou";
        	char[] chars = { 'a', 'e', 'i', 'o', 'u' };
        	System.out.println("Longest Count = " + longestSubSequence(str, chars));	
+       	
+     // ############TEST 4 ###################
+        int[] a = {4, 1, 2, 3}; 
+        System.out.println("Max Diff : " +  maxDiff(a, a.length));
 	}
 
+	private static void createAuditLog() {
+		ExecutorService pool = Executors.newFixedThreadPool(4); 
+		List<Future<String>> futures =new ArrayList<Future<String>>(1);
+		for(int i = 0; i < 1; i++) {
+			System.out.println("Submitting task " + i);
+			futures.add(pool.submit(new StringTask()));
+		};
+
+		//Compute the result} 
+		for(Future<String> future : futures){
+			try {
+				String result = future.get();
+				System.out.println(result);
+			} catch (InterruptedException | ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		pool.shutdown();
+	}
+
+	static int maxDiff (int arr[], int n)
+	{
+	    // Initialize diff, current sum and max sum
+	    int diff = arr[1]-arr[0];
+	    int curr_sum = diff;
+	    int max_sum = curr_sum;
+	 
+	    for(int i=1; i<n-1; i++)
+	    {
+	        // Calculate current diff
+	        diff = arr[i+1]-arr[i];
+	 
+	        // Calculate current sum
+	        if (curr_sum > 0)
+	           curr_sum += diff;
+	        else
+	           curr_sum = diff;
+	 
+	        // Update max sum, if needed
+	        if (curr_sum > max_sum)
+	           max_sum = curr_sum;
+	    }
+	 
+	    return max_sum;
+	}
+	
 	static int longestSubSequence(String s, char[] c) {
 
 	    //exit condition
@@ -88,12 +171,45 @@ public class ChaseDigital {
 	    }
 	}
 	
+    static String[] returnMovieTitles(String jsonStr) {
+    	List<String> titlesList = new ArrayList<String>();
+    	
+    	JsonParser parser = new JsonParser();
+        JsonObject o = parser.parse(jsonStr).getAsJsonObject();
+        /*JsonElement title = o.get("data")
+                .getAsJsonArray().get(0)
+                .getAsJsonObject().get("Title");
+        System.out.println("Title = " + title);*/
+        
+        JsonArray titleJsonArr =  o.get("data")
+                .getAsJsonArray();
+        titleJsonArr.forEach(i ->
+		{
+			//System.out.println("data : " + i);
+			JsonElement title = i.getAsJsonObject().get("Title");
+			//System.out.println("Title = " + title);
+			
+			titlesList.add(title.getAsString());
+			
+			/*if("E".equals(k)){
+				System.out.println("Hello E");
+			}*/
+		});
+        
+        System.out.println("Read List = " + titlesList);
+        java.util.Collections.sort(titlesList);
+        
+        String[] titlesArr = new String[titlesList.size()];
+        titlesArr = titlesList.toArray(titlesArr);
+        
+        return titlesArr;
+    }
+    
     static String[] getMovieTitles(String substr) {
         Gson gson = new Gson();
 
         //JsonObject jsonObj = element.getAsJsonObject();
-
-        
+     
         try {
         	//String sURL = "http://freegeoip.net/json/"; //just a string
         	//String sURL = "http://api.coindesk.com/v1/bpi/currentprice.json";
